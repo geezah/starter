6168a4797b9178605357471204661f0ee4fa4bc6
geezah <emre.gezer@hotmail.de>
2026-07-19 19:55:50 +0200

    configure ruff for python

---
+++ lua/plugins/ruff.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        python = {
+          "ruff_fix",
+          "ruff_organize_imports",
+          "ruff_format",
+        },
+      },
+    },
+  },
+}
